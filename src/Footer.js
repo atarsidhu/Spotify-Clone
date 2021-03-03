c3fc97fd65e06dd208eae21526111501d4d35b8b
Atar Sidhu <a.sidhu14@hotmail.com>
2021-03-02 16:43:32 -0800

    test

--- src/Footer.js
+++ src/Footer.js
@@ -14,6 +14,7 @@ import { songSelected } from "./SongRow";
 function Footer() {
   //   const [{ discover_weekly }, dispatch] = useDataLayerValue();
   //   const songName = songSelected();
+  //
 
   return (
     <div className="footer">
